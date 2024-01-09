d51312aa72b8fd1f206516145f4d57c9c49c2e34
RomanKosmyna <roma.kosmyna@gmail.com>
2024-01-09 14:17:52 +0200

    Added methods to get and evaluate discriminants in Discriminant class.

--- QuadraticEquation/Discriminant.cs
+++ QuadraticEquation/Discriminant.cs
@@ -30,4 +30,35 @@
             Console.WriteLine(ex.Message);
         }
     }
+
+    public double GetDiscriminant()
+    {
+        double discriminant = Math.Pow(_b, 2) - 4 * _a * _c;
+
+        return discriminant;
+    }
+
+    public void EvaluateDiscriminant(double discriminant)
+    {
+        if (discriminant > 0)
+        {
+            double x1 = (-_b + Math.Sqrt(discriminant)) / (2 * _a);
+            double x2 = (-_b - Math.Sqrt(discriminant)) / (2 * _a);
+
+            Console.WriteLine($"Два різних реальних корені: x1 = {x1}, x2 = {x2}");
+        }
+        else if (discriminant == 0)
+        {
+            double x = -_b / (2 * _a);
+
+            Console.WriteLine($"Один реальний корінь (кратності 2): x = {x}");
+        }
+        else
+        {
+            double realPart = -_b / (2 * _a);
+            double imaginaryPart = Math.Sqrt(Math.Abs(discriminant)) / (2 * _a);
+
+            Console.WriteLine($"Два комплексних корені: x1 = {realPart} + {imaginaryPart}i, x2 = {realPart} - {imaginaryPart}i");
+        }
+    }
 }
